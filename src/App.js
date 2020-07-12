7bd75ea1184963424ae9e56001e938be5d2cea62
My Name <tranhoang.finizz@gmail.com>
2020-07-12 13:24:10 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Mess from './components/mess/mess'
+import Mess1 from './components/mess1/mess1'
 function App() {
   return (
     <div className="App">
       <Mess />
+      <Mess1 />
     </div>
   );
 }
---
+++ src/components/mess1/mess1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+function mess1() {
+    return (
+        <MessengerCustomerChat
+        pageId="110463107407022"
+        appId="275939346842688"
+        />
+    )
+}
+
+export default mess1
